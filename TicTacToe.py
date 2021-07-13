64d9ecf130a88b0dea21480a02ccca1acc17b77d
Ace1294 <83032267+13151294@users.noreply.github.com>
2021-07-13 14:59:24 +0700

    tic tac toe 1.1

--- TicTacToe.py
+++ TicTacToe.py
@@ -2,11 +2,11 @@
 #Compute by Ace Fhid
 from random import choice
 
+x, o = '\033[038;2;255;102;102mX\033[0;0m', '\033[038;2;102;199;255mO\033[0;0m'
 Board = {
     '7':' ', '8':' ', '9':' ',
     '4':' ', '5':' ', '6':' ',
-    '1':' ', '2':' ', '3':' ',
-}
+    '1':' ', '2':' ', '3':' ',}
 def DisplayBoard(board):
     print('\033[038;2;255;175;36m{}\033[038;2;255;175;36m|{}\033[038;2;255;175;36m|{}'.format(board['7'], board['8'], board['9']))
     print('\033[038;2;255;175;36m-+-+-')
@@ -14,22 +14,15 @@ def DisplayBoard(board):
     print('\033[038;2;255;175;36m-+-+-')
     print('\033[038;2;255;175;36m{}\033[038;2;255;175;36m|{}\033[038;2;255;175;36m|{}\033[0;0m'.format(board['1'], board['2'], board['3']))
 def CheckWin():
-    win = False
-    if Board['7'] == Board['8'] == Board['9'] != ' ' or Board['4'] == Board['5'] == Board['6'] != ' ' or Board['1'] == Board['2'] == Board['3'] != ' ':
-        win = True
-    elif Board['8'] == Board['5'] == Board['2'] != ' ' or Board['7'] == Board['4'] == Board['1'] != ' ' or Board['9'] == Board['6'] == Board['3'] != ' ':
-        win = True
-    elif Board['7'] == Board['5'] == Board['3'] != ' ':
-        win = True
-    elif Board['9'] == Board['5'] == Board['1'] != ' ':
-        win = True
-    return win
+    return (Board['7'] == Board['8'] == Board['9'] != ' ') or (Board['4'] == Board['5'] == Board['6'] != ' ') or (Board['1'] == Board['2'] == Board['3'] != ' ') or (Board['8'] == Board['5'] == Board['2'] != ' ') or (Board['7'] == Board['4'] == Board['1'] != ' ') or (Board['9'] == Board['6'] == Board['3'] != ' ') or (Board['7'] == Board['5'] == Board['3'] != ' ') or (Board['9'] == Board['5'] == Board['1'] != ' ')
 def repeat():
-    Repeat = input("Wanna Repeat(y/n):")
+    Repeat = input("\033[038;2;128;230;255mWanna Repeat(y/n) : ")
     if Repeat.lower() == 'y' or repeat == 1:
+        for i in Board.keys():
+            Board[i] = ' '
         game()
-    elif Repeat.lower() == 'y' or repeat == 1:
-        print("Good Bye")
+    elif Repeat.lower() == 'n' or repeat == 0:
+        print("\033[038;2;210;156;255mGood Bye\033[0;0m")
 def Player(turn):
     DisplayBoard(Board)
     running = True
@@ -48,15 +41,13 @@ def Player(turn):
         else:
             filled = True
 def game():
-    XO = {'\033[038;2;255;102;102mX\033[0;0m':'\033[038;2;102;199;255mO\033[0;0m', '\033[038;2;102;199;255mO\033[0;0m':'\033[038;2;255;102;102mX\033[0;0m'}
+    
+    XO = {x:o, o:x}
     turn = choice(list(XO.keys()))
     run = True
     count = 0
     while run:
-        if turn == '/033[038;2;255;187;0mX/033[0;0m':
-            move = Player(turn)
-        else:
-            move = Player(turn)
+        move = Player(turn)
         count += 1
         Board[move] = turn
         win = CheckWin()
@@ -64,10 +55,11 @@ def game():
             DisplayBoard(Board)
             print("\n\033[038;2;255;175;36m-{} \033[038;2;255;175;36mWin-\n\033[0;0m".format(turn))
             run = False
-        if all(Board[i] != ' ' for i in Board.keys()):
+        if all(Board[i] != ' ' for i in Board.keys()) and not win:
             DisplayBoard(Board)
             print("\n\033[038;2;255;175;36m-Draw!-\n\033[0;0m".format(turn))
             run = False
         turn = XO[turn]
     repeat()
-game()
+if __name__ == "__main__":
+    game()
